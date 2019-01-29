474e675ec2a283f921e30f6f013913c86cb2b5fb
Pimpys <maxkyivua@gmail.com>
2019-01-29 20:03:11 +0200

    add function

--- config/function.php
+++ config/function.php
@@ -11,3 +11,39 @@
  * Date: 29.01.2019
  * Time: 19:23
  */
+
+/**
+ * @param string $string
+ * @return string
+ */
+function clearData(string $string)
+{
+    return strip_tags(trim($string));
+}
+
+/**
+ * @param string $string
+ * @return bool|int|string
+ */
+function checkDataLength(string $string)
+{
+    $string = strlen(clearData($string));
+    if (!empty($string) && $string <= 254){
+        return $string;
+    }
+    return false;
+}
+
+/**
+ * @param string $string
+ * @return bool|string
+ */
+function checkEmail(string $string)
+{
+    $pattern = "#^[-0-9a-z_\.]+@[0-9a-z_^\.]+\.[a-z]{2,6}$#i";
+    if (!preg_match($pattern, clearData($string)))
+    {
+        return false;
+    }
+    return $string;
+}
