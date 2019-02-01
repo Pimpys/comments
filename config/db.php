f1d2c25b1105a26c6abbd93c312fc5535ace7999
Pimpys <maxkyivua@gmail.com>
2019-02-01 10:51:22 +0200

    connect to bd

--- config/db.php
+++ config/db.php
@@ -11,3 +11,13 @@
  * Date: 29.01.2019
  * Time: 19:22
  */
+$dns = 'mysql:host=localhost;dbname=comments;charset=UTF8';
+$username = 'root';
+$passwd = 'password';
+
+try{
+    $pdo = new PDO($dns, $username, $passwd);
+}catch (PDOException $e){
+    echo 'Подключение не удалось: ' . $e->getMessage();
+    die;
+}
